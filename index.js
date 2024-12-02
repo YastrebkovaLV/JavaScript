708eeb0013ff2fa39b893e9fa135a8e518daf9ba
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-02 14:29:12 +0300

    Первый и последний символ строки (2 задание)

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ const firstChar = textExample[0].toUpperCase()
 const lastChar = textExample[textExample.length - 1].toUpperCase()
 const middlePart = textExample.substring(1, textExample.length - 1);
 
+console.log(`${firstChar}${middlePart}${lastChar}`);
