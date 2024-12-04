d6487d0d48d855a4c2d602004ef672b0383061b2
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-04 20:20:28 +0300

    FizzBuzz (7 задание, условия)

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-// Вложенные условия (6 задание, условия)
+// FizzBuzz (7 задание, условия)
 
-const number = -0.0055;
+const number = 666;
 
-if (number === 0) {
-    console.log("A");
+if (number % 3 === 0 && number % 5 === 0) {
+    console.log("FizzBuzz");
 }
-else if (number > 1) {
-    if (number % 2 === 0) {
-        console.log("B");
-    }
-    else {
-        console.log("C");
-    }
+else if (number % 3 === 0) {
+    console.log("Fizz");
 }
-
-else if (number < -1) {
-    if (number % 2 === 0) {
-        console.log("D");
-    }
-    else {
-        console.log("E");
-    }
+else if (number % 5 === 0) {
+    console.log("Buzz");
 }
 else {
-    const lastNumber = Math.abs(number).toString().slice(-1);
-    if (lastNumber === '5') {
-        console.log("F");
-    }
-
-    else if (lastNumber % 2 === 0) {
-        console.log("G");
-    }
-    else {
-        console.log("H");
-    }
+    console.log(number);
 }
