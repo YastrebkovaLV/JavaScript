4092b038794ac7988b55de501a6c8e713ad94cfd
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-04 10:29:09 +0300

    Одно число (2 задание,условия)

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-// Площадь квадрата
+// Одно число (2 задание,условия)
 
-const a = 4;
-const square = a * a;
-console.log(`Площадь квадрата : ${square}`);
+const number = 122;
+
+if (number % 1 === 0) {
+    console.log("Число целое");
+}
+else {
+    console.log("Число не целое");
+}
+
+if (number % 2 === 0) {
+    console.log("Число четное ")
+}
+else {
+    console.log("Число не четное")
+}
+
+const numStr = number.toString();
+const firstNumber = parseInt(numStr[0]);
+const lastNumber = parseInt(numStr[numStr.length - 1]);
+
+const sum = firstNumber + lastNumber;
+console.log(` Сумма первой и последней цифры : ${sum}`);
