f38d447f1bce45696b734cb1617dca2d57d73c0b
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-04 19:36:21 +0300

    Одна строка (4 задание,условия)

--- index.js
+++ index.js
@@ -1,48 +1,24 @@
-// Два числа (3 задание,условия)
+// Одна строка (4 задание,условия)
 
-const number = 123;
-const number2 = 22;
+const stroka = "Я изучаю джаваскрипт !";
 
-// Равенство
+// Первый символ ,если длина строки четная
 
-if (number === number2) {
-    console.log("Числа равны");
+if (stroka.length % 2 === 0) {
+    console.log(stroka[0]);
+    console.log(" Первый символ,длина четная ");
 }
+
+// Предпоследний символ, если длина строки нечетная и строка оканчивается на букву "а"
+
+else if (stroka.length % 2 !== 0 && stroka[stroka.length - 1] === 'а') {
+    console.log(stroka[stroka.length - 2]);
+    console.log(" Предпоследний символ,ваша строка заканчивается на 'а' либо нечетная ");
+}
+
+// Последний символ, если длина строки нечетная
+
 else {
-    console.log("Числа не равны");
-}
-
-// Большее и меньшее
-
-if (number > number2) {
-    console.log(`Большее число: ${number}`);
-    console.log(`Меньшее число: ${number2}`);
-}
-else if (number < number2) {
-    console.log(`Большее число: ${number2}`);
-    console.log(`Меньшее число: ${number}`);
-}
-else {
-    console.log("Числа равны");
-}
-
-// Проверка делимости
-
-if (number !== 0 && number2 % number === 0) {
-    console.log(`${number2} делится на ${number} без остатка`);
-}
-else if (number2 !== 0 && number % number2 === 0) {
-    console.log(`${number} делится на ${number2} без остатка `);
-}
-else {
-    console.log(" Ничего не делится ");
-}
-
-// Количество цифр
-
-if (String(number).length === String(number2).length) {
-    console.log(" Одинаковое количество ");
-}
-else {
-    console.log(" Разное количество ");
+    console.log(stroka[stroka.length - 1]);
+    console.log(" Длина нечетная ")
 }
