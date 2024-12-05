583abbba6eb8953b445181441e7eda408fd04fd5
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-05 11:35:46 +0300

    Случайность (1 задание, while)

--- index.js
+++ index.js
@@ -1,48 +1,16 @@
-// Базовые операции (1 задание, массивы)
+// Случайность (1 задание, while)
 
-const arr = [10,11,33,-1,22,3,-4]; // мой массив
+let number = 2;
 
-console.log(arr[0]); // первое значение
+let randomNumber;
 
-console.log(arr[arr.length-1]); // последнее значение
+while (true) {
+    randomNumber = Math.random();
+    number++;
 
-const size = arr.length; // размер
+    if (randomNumber > 0.95) {
+        break;
+    }
+}
 
-arr.push(400); //новые значения в конце  массива
-
-console,log(arr[arr.length-1]); // последнее значение
-
-arr.shift(); // удалить первое значение
-
-console.log(arr[0]); // первое значение
-
-arr[arr.length-1] = 200; //перезапись на новое значение
-
-arr.push(200); // новое зачение в конце массива
-
-let index = Math.floor(arr.length / 2); // Индекс
-console.log(arr[index]);
-
-arr[0] = "Я не знаю";       // Перезапись на новое значение
-arr[arr.length-1] = "Я не знаю";
-arr[index] = "Я не знаю";
-
-console.log(arr.indexOf("Я не знаю")); // Первый и последний индекс
-console.log(arr.lastIndexOf("Я не знаю"));
-
-console.log(arr.includes(1)); // есть ли один в массиве
-
-console.log(arr.length); // длина
-
-console.log(arr.length - size); // разница в размере
-
-let obj = { name: 'Lilia',nick: []}; // это обьект
-
-arr.push(obj); // обьект в массиве
-
-arr[arr.length-1].nick = []; // новое поле с пустым массивом
-
-arr[arr.length-1].nick.push('nick1');
-arr[arr.length-1].nick.push('nick2'); // новое значение
-
-console.log(arr); // вывод массива
+console.log(number);
