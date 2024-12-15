dfc82d36df79e25b9bbb78b7fdca60cce66af3a2
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-15 19:55:09 +0300

    Подсчет (9 задание , цикл for )

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-// Вывести вторую половину (8 задание , цикл for )
+// Подсчет (9 задание , цикл for )
 
-let a = ["bbb","bbbbbb","bbbbb","bbbbbb","bbbbb","bbbbbbbb","bbbbbbb","bbbb"];
-let index = Math.floor(a.length/2);
+let a = [1,3,4,5,-3,-555,-2,-5];
+let plus = 0;
+let minus = 0;
 
-for (let i = index ; i < a.length; i++) {
-    console.log(a[i]);
+for (let i = 0; i < a.length -1; i++) {
+    if(a[i] > 0) {
+        plus++;
+    }
+    else if (a[i] < 0) {
+        minus++;
+    }
 }
+
+console.log(`Положительных : ${plus}`);
+console.log(`Отрицательных : ${minus}`);
