d477891b5b13443405bd768e09a5cf4604c23750
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-15 19:06:07 +0300

    Вывод чисел (1 задание, цикл for )

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-// Случайность (1 задание , while)
+// Вывод чисел (1 задание , цикл for )
 
-let a = 1;
-let randomNumber = Math.random();
+// от 0 до 100
 
-while (randomNumber <= 0.95 ) {
-    a++;
-    randomNumber = Math.random();
+for (let i = 0; i <= 100; i++) {
+    console.log(i);
 }
 
-console.log(`количество попыток: ${a}`);
+// от -100 до 0
+
+for (let i = -100 ; i <= 0; i++) {
+    console.log(i);
+}
+
+// от 100 до 0
+
+for (let i = 100 ; i >= 0; i--) {
+    console.log(i);
+}
+
+// Четные числа
+
+for (let i = 0 ; i <= 100; i+= 2) {
+    console.log(i);
+}
+
+// Вывод чисел 0 и 100, делятся на 3 или на 5
+
+for (let i = 0; i <+ 100; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+        console.log(i);
+    }
+}
+
+// Вывод между 0 и 1000, сумма первой и последней цифры равна 10
+
+for (let i = 0; i <= 1000; i++) {
+    let num = i.toString();
+    let first = Number(num[0]);
+    let last = Number(num[num.length - 1]);
+    if (first + last === 10) {
+        console.log(i);
+    }
+}
