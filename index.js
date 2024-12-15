b4201ad9bc8aac08b21f38ad44df877bf19a382a
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-15 19:58:02 +0300

    Угол (10 задание , цикл for )

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-// Подсчет (9 задание , цикл for )
+// Угол (10 задание , цикл for )
 
-let a = [1,3,4,5,-3,-555,-2,-5];
-let plus = 0;
-let minus = 0;
-
-for (let i = 0; i < a.length -1; i++) {
-    if(a[i] > 0) {
-        plus++;
-    }
-    else if (a[i] < 0) {
-        minus++;
-    }
+for(let i =9; i > 0; i--) {
+    console.log('*'.repeat(i));
 }
-
-console.log(`Положительных : ${plus}`);
-console.log(`Отрицательных : ${minus}`);
