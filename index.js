1eeca1e49bedb8abfe82da55779af6a2259ec909
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-15 20:18:18 +0300

    Вывод матрицы (13 задание , цикл for )

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-// Ёлочка (12 задание , цикл for )
+// Вывод матрицы (13 задание , цикл for )
 
-let a = 5;
+ const a = [
+    ['Id', 'Title', 'Description'],
+    [1, 'Title-1', 'Description-1'],
+    [2, 'Title-2', 'Description-2'],
+    [3, 'Title-3', 'Description-3', 'Поле с багом'],
+    [4, 'Title-4', 'Description-4'],
+    [5, 'Title-5', 'Description-5'],
+    [6, 'Title-6', 'Description-6', 'Поле с багом'],
+];
 
-for (let i = 0; i < a; ++i) {
-    let star = '*'.repeat(2*i + 1);
-    let probel = ' '.repeat(a - i - 1);
-    console.log(star+probel);
+for (let i = 0; i < a.length; i++) {
+    let b = '';
+    for (let u = 0; u < a[i].length; u++) {
+        b += a[i][u] + '\t';
+    }
+    console.log(b.trim());
 }
