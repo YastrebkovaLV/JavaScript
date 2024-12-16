8697c0dae2547127532b1943af2e9dc78741f100
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-16 11:10:13 +0300

    Уникальные элементы (5 задание , цикл for of)

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-// Общие элементы (4 задание , цикл for of)
+// Уникальные элементы (5 задание , цикл for of)
 
-const arr1 = ['Ivan','Kovan',0,'D'];
-const arr2 = ['Kovan','NeIvan',1,0,'C'];
+const arr1 = ['Ivan','Kovan','C',0,1];
+const arr2 = ['Kovan','NeIvan','D',1,3];
 
 const common = [];
 
 for (const arr of arr1) {
-    if (arr2.includes(arr)) {
+    if (!arr2.includes(arr) && !common.includes(arr)) {
         common.push(arr);
     }
 }
+    for (const arr of arr2) {
+        if (!arr1.includes(arr) && !common.includes(arr)) {
+            common.push(arr);
+        }
+    }
 
-console.log(common);
+    console.log(common);
