2681553db83634d1db47d5b9c92e418c7594e8ba
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-16 10:59:00 +0300

    Общие элементы (4 задание , цикл for of)

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-//  Сравнение массивов (3 задание , цикл for of)
+// Общие элементы (4 задание , цикл for of)
 
-const arr1 = ['Ivan', 'Kovan'];
-const arr2 = ['Kovan', 'Ivan'];
+const arr1 = ['Ivan','Kovan',0,'D'];
+const arr2 = ['Kovan','NeIvan',1,0,'C'];
 
-const arr3 = ['A'];
-const arr4 = ['A', 'B'];
+const common = [];
 
-const arr5 = ['C', 1, false];
-const arr6 = [false, 1, 'C'];
-
-const arr7 = [null, 0, 'D'];
-const arr8 = ['D', null];
-
-const arr9 = [];
-const arr10 = [];
-
-const arr11 = ['0'];
-const arr12 = [0];
-
-let a = (arr1, arr2) => {
-    if (arr1.length !== arr2.length) return false;
-
-    let b = true;
-
-    for (const item of arr1) {
-        if (!arr2.includes(item)) {
-            b = false;
-            break;
-        }
+for (const arr of arr1) {
+    if (arr2.includes(arr)) {
+        common.push(arr);
     }
+}
 
-    return b;
-};
-
-console.log(a(arr1, arr2));
-console.log(a(arr3, arr4));
-console.log(a(arr5, arr6));
-console.log(a(arr7, arr8));
-console.log(a(arr9, arr10));
-console.log(a(arr11, arr12));
+console.log(common);
