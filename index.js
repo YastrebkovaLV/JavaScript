a781a863a64bdbfd3a022c4c6e065019f823ec86
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-16 10:50:23 +0300

    Сравнение массивов (3 задание , цикл for of)

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-// Гласные буквы (2 задание , цикл for of)
+//  Сравнение массивов (3 задание , цикл for of)
 
-let slovo = "хрюхрю_хахаахахахха";
-const abc = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е'];
-let a = 0;
-let b = 0;
+const arr1 = ['Ivan', 'Kovan'];
+const arr2 = ['Kovan', 'Ivan'];
 
-for (const char of slovo) {
-    if (abc.includes(char.toLowerCase())) {
-        a++;
-    } else if (char.match(/[а-яё]/i)) {
-        b++;
+const arr3 = ['A'];
+const arr4 = ['A', 'B'];
+
+const arr5 = ['C', 1, false];
+const arr6 = [false, 1, 'C'];
+
+const arr7 = [null, 0, 'D'];
+const arr8 = ['D', null];
+
+const arr9 = [];
+const arr10 = [];
+
+const arr11 = ['0'];
+const arr12 = [0];
+
+let a = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) return false;
+
+    let b = true;
+
+    for (const item of arr1) {
+        if (!arr2.includes(item)) {
+            b = false;
+            break;
+        }
     }
-}
 
-console.log(`Гласные: ${a}, Негласные: ${b}`);
+    return b;
+};
+
+console.log(a(arr1, arr2));
+console.log(a(arr3, arr4));
+console.log(a(arr5, arr6));
+console.log(a(arr7, arr8));
+console.log(a(arr9, arr10));
+console.log(a(arr11, arr12));
