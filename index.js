f3c4856a1c40c87763d07dd0b31a7cc41ffef408
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-16 11:14:42 +0300

    Строка в массив (6 задание , цикл for of)

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-// Уникальные элементы (5 задание , цикл for of)
+// Строка в массив (6 задание , цикл for of)
 
-const arr1 = ['Ivan','Kovan','C',0,1];
-const arr2 = ['Kovan','NeIvan','D',1,3];
+const word = "a car";
+const arr = [];
 
-const common = [];
+for (const char of word) {
+    arr.push(char);
 
-for (const arr of arr1) {
-    if (!arr2.includes(arr) && !common.includes(arr)) {
-        common.push(arr);
-    }
 }
-    for (const arr of arr2) {
-        if (!arr1.includes(arr) && !common.includes(arr)) {
-            common.push(arr);
-        }
-    }
 
-    console.log(common);
+console.log(arr);
