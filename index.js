6873614a387530b68c6ffe01fba8a32c8f56badc
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-16 11:30:05 +0300

    Умножение числовых полей (3 задание , цикл for in)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// Вывод пар ключ-значение (2 задание , цикл for in)
+// Умножение числовых полей (3 задание , цикл for in)
 
-const person = {age:10 ,name:' ', child: 'Max'};
+const person = { age:10 , name: "", credit: 100};
 
 for (let key in person) {
-    console.log(`${key}: ${person[key]}`);
+    person[key] *= typeof person[key] === "number" ? 1.2 : 1;
 }
+
+console.log(person);
