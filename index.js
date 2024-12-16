49164b58fc4ab7889c923027c7523ac6461dd175
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-16 11:33:56 +0300

    Вывод ключей через запятую (4 задание , цикл for in)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-// Умножение числовых полей (3 задание , цикл for in)
+// Вывод ключей через запятую (4 задание , цикл for in)
 
-const person = { age:10 , name: "", credit: 100};
+const person = {age: 10, name: "", child: null};
+let keys = '';
 
 for (let key in person) {
-    person[key] *= typeof person[key] === "number" ? 1.2 : 1;
+    keys += key + ',';
 }
 
-console.log(person);
+keys = keys.slice(0,-2);
+
+console.log(keys);
