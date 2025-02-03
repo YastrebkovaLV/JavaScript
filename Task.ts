bd2733f2318df0993c3be0743c8ddf2164f073fd
Lilia Yastrebkova <kekovichl436@gmail.com>
2025-02-03 15:31:01 +0300

    Микс (задание 1)

--- Task.ts
+++ Task.ts
@@ -1,5 +1,20 @@
-// 1 - 1,3,7,11
-// 2 - 4,6,8,10
-// 3 - 2,5,9,12
+type MyType = {
+  optionalField?: string | null;
+};
+
+const object1: MyType = {};
+const object2: MyType = { optionalField: null };
+const object3: MyType = { optionalField: '' };
+const object4: MyType = { optionalField: 'нормальная строка' };
+
+const printField = (obj: MyType) => {
+  const fieldValue = obj.optionalField ?? 'не обнаружено';
+  console.log(fieldValue,'пусто');
+};
+
+printField(object1);
+printField(object2);
+printField(object3);
+printField(object4);
 
 
