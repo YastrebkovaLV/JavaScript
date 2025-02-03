a0e4df022ea8d9a15bf95a4779d987aa89e5bd17
Lilia Yastrebkova <kekovichl436@gmail.com>
2025-02-03 15:14:43 +0300

    ISO8601 (задание 5)

--- Task.ts
+++ Task.ts
@@ -1,12 +1,5 @@
-type Obj = {
-  createdAt: Date;
-  createdAtISO: string;
-};
+// 1 - 1,3,7,11
+// 2 - 4,6,8,10
+// 3 - 2,5,9,12
 
-const myObj: Obj = {
-  createdAt: new Date(),
-  createdAtISO: new Date().toISOString(),
-}
-
-console.log(myObj);
 
