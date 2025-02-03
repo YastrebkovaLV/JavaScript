c6332aa625d4529848d70184c83cb910e3dd7db8
Lilia Yastrebkova <kekovichl436@gmail.com>
2025-02-03 15:35:41 +0300

    Что выведется? (задание 2)

--- Task.ts
+++ Task.ts
@@ -1,20 +1,3 @@
-type MyType = {
-  optionalField?: string | null;
-};
+// const a = ('' || null || 0 || -0 || undefined) ?? ((null || 0) ?? '' ?? null);
 
-const object1: MyType = {};
-const object2: MyType = { optionalField: null };
-const object3: MyType = { optionalField: '' };
-const object4: MyType = { optionalField: 'нормальная строка' };
-
-const printField = (obj: MyType) => {
-  const fieldValue = obj.optionalField ?? 'не обнаружено';
-  console.log(fieldValue,'пусто');
-};
-
-printField(object1);
-printField(object2);
-printField(object3);
-printField(object4);
-
-
+// const a = (undefined) ?? (0);
