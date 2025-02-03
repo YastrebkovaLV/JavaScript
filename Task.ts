cf3f39bfb50ad752616dffe2e2f65a47a09481b5
Lilia Yastrebkova <kekovichl436@gmail.com>
2025-02-03 14:29:38 +0300

    Строковой литерал (задание 1)

--- Task.ts
+++ Task.ts
@@ -1,18 +1,5 @@
-import {blue, green, red} from 'chalk';
+type Status = "success" | "failure";
 
-const aaa = 15;
+const st: Status = "success";
 
-const result = [];
-
-const color = [red,green, blue];
-
-for (let i = 0; i < aaa; i++)
-{
-
-  const color2 = color[i % 3];
-  result.push(color2('*'));
-
-
-}
-
-console.log(result.join(' - '));
+console.log(`Статус: ${st}`);
